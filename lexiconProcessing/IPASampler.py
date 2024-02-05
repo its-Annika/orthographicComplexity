094ae75ebda8711177937c366570707c92244d6c
Annika Shankwitz <annikashankwitz@Interstellar.local>
2024-02-05 16:55:22 -0500

    updates

--- lexiconProcessing/IPASampler.py
+++ lexiconProcessing/IPASampler.py
@@ -1,11 +1,11 @@
 import sys
 import re
-import random 
+import random
 
 pairs = sys.stdin.readlines()
 storage = []
 seenWords = []
-
+exemplars = str(sys.argv[1])
 
 currentLetter = pairs[1][0].lower()
 
@@ -13,7 +13,25 @@ for pair in pairs:
 
 	orthForm = pair.split("\t")[0].lower()
 	ipaForm = re.sub("\s", "", pair.split("\t")[1])
-	
+
+	#throw out words over 25 characters	
+	if len(orthForm) > 25:
+		continue
+
+	#prevent pronunciation of acronyms: <who> as "dʌbəljuːeɪt͡ʃəʊ"
+	if len(orthForm) > len(ipaForm) * 2:
+		continue
+
+	#throw out words with invalid characters for the given langauge 
+	invalid = False
+	for character in orthForm:
+		if character not in exemplars:
+			invalid = True
+			break
+
+	if invalid:
+		continue
+			
 
 	#keep only one pronucition of each word
 	if orthForm in seenWords:
