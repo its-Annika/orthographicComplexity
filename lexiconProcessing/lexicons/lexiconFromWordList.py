1b7f1f5c2d3fcc953aca154b32dbb339ad468f3d
Annika Shankwitz <annikashankwitz@Interstellar.local>
2024-02-09 17:27:46 -0500

    lexicon stuff

--- lexiconProcessing/lexicons/lexiconFromWordList.py
+++ lexiconProcessing/lexicons/lexiconFromWordList.py
@@ -10,12 +10,15 @@ line = sys.stdin.readline()
 while line:
 	
 	formated = line.strip().lower()
-	
+
 	valid = True
-	for character in formated:
-		if character not in validCharacters:
-			valid = False
-			break
+	
+	if(validCharacters != "pass"):
+
+		for character in formated:
+			if character not in validCharacters:
+				valid = False
+				break
 	
 	if valid:
 		sys.stdout.write(formated + "\t" + epi.transliterate(formated) + "\n")
