85b3dd1a06c85a568d1d1b6add0255344bd27035
its-Annika <ashankwi@ehecatl.cl.indiana.edu>
2024-04-14 10:29:32 -0400

    bash scripts

---
+++ ASR_Data/makeSets.sh
@@ -0,0 +1,4 @@
+
+python3 makeSets2.py /home/ashankwi/commonVoiceData/$1/trainFiles/ /home/ashankwi/commonVoiceData/$1/train.tsv  $1 Train
+python3 makeSets2.py /home/ashankwi/commonVoiceData/$1/testFiles/ /home/ashankwi/commonVoiceData/$1/test.tsv $1 Test
+python3 makeSets2.py /home/ashankwi/commonVoiceData/$1/clips/ /home/ashankwi/commonVoiceData/$1/dev.tsv $1 Dev
---
+++ ASR_Data/prepareData.sh
@@ -0,0 +1,10 @@
+
+python3 prepareData.py /home/ashankwi/orthographicComplexity/ASR_Data/$1ASR_Files/$11hourTrain.txt $11Train
+python3 prepareData.py /home/ashankwi/orthographicComplexity/ASR_Data/$1ASR_Files/$12.5hourTrain.txt $12.5Train
+python3 prepareData.py /home/ashankwi/orthographicComplexity/ASR_Data/$1ASR_Files/$15hourTrain.txt $15Train
+python3 prepareData.py /home/ashankwi/orthographicComplexity/ASR_Data/$1ASR_Files/$110hourTrain.txt $110Train
+
+python3 prepareData.py /home/ashankwi/orthographicComplexity/ASR_Data/$1ASR_Files/$11hourDev.txt $11Dev
+
+python3 prepareData.py /home/ashankwi/orthographicComplexity/ASR_Data/$1ASR_Files/$11hourTest.txt $11Test
+python3 prepareData.py /home/ashankwi/orthographicComplexity/ASR_Data/$1ASR_Files/$15hourTest.txt $15Test
