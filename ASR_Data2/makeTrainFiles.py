089b5af5eaa7f84fdd63a79a04ac3d5daef2d825
its-Annika <ashankwi@ehecatl.cl.indiana.edu>
2024-06-07 15:34:39 -0400

    more efficient

--- ASR_Data2/makeTrainFiles.py
+++ ASR_Data2/makeTrainFiles.py
@@ -15,10 +15,10 @@ audioFolderPath = sys.argv[2]
 speakerNumber = int(sys.argv[3])
 langCode = sys.argv[4]
 
-durationDict = {}
-#{speakerID, totalDuration}
+numFilesDict = {}
+#{speakerID, # of files they have}
 speakerDict = {}
-#{speakerID, [(path,transcript,duration),(),(),]
+#{speakerID, [(path,transcript),(),(),]
 
 with open(trainFilePath) as tf:
 	next(tf)
@@ -27,28 +27,27 @@ with open(trainFilePath) as tf:
 		path = line.split("\t")[1]
 		transcript = line.split("\t")[2]
 		cleanTranscript = re.sub("[.,!?\"\'`#@$^&*\(\)\[\]]", "", transcript).lower()
-		audio = MP3(audioFolderPath+path)
-		duration = audio.info.length
 
-		attributes = (audioFolderPath+path,cleanTranscript,duration)
+		attributes = (audioFolderPath+path,cleanTranscript)
 
 		if id in speakerDict.keys():
 			speakerDict[id].append(attributes)
-			durationDict[id] += duration
+			numFilesDict[id] += 1
 		else:
 			speakerDict[id] = [attributes]
-			durationDict[id] = duration
+			numFilesDict[id] = 0
 
 
 
 #pull the files as equally as possible from the chosen number of speakers
-speakerPool = sorted(durationDict, key=durationDict.get, reverse=True)[:speakerNumber+1]
+speakerPool = sorted(numFilesDict, key=numFilesDict.get, reverse=True)[:speakerNumber+1]
 
 totalDuration = 0
 
 dataSet = []
 
 speaker = 0
+count = 0
 
 while totalDuration < 36000:
 	if len(speakerDict[speakerPool[speaker]]) != 0:
@@ -57,22 +56,28 @@ while totalDuration < 36000:
 		temp = speakerDict[speakerPool[speaker]]
 		temp.remove(selected)
 		speakerDict[speakerPool[speaker]] = temp
-		dataSet.append(selected)
-		totalDuration += selected[2]
+		audio = MP3(selected[0])
+		totalDuration += audio.info.length
+		dataSet.append((selected[0], selected[1], audio.info.length))
 
 	if speaker == speakerNumber:
 		speaker = 0
 	else:
 		speaker += 1
 
+	count+=1
+
+	if count % 500 == 0:
+		print(totalDuration)
+
 #print the dataFiles
 totalDuration = 0
 with open(langCode+"2.5HourTrain.txt", "w+") as two, open(langCode+"5HourTrain.txt", "w+") as five, open(langCode+"10HourTrain.txt", "w+") as ten:
 	for path, transcript, duration in dataSet:
 		totalDuration += duration
-		if totalDuration < 9000:
-			two.write(path+","+transcript+"\n")
-		if totalDuration < 18000:
-			five.write(path+","+transcript+"\n")
-		if totalDuration < 36000:
-			ten.write(path+","+transcript+"\n")
+		if totalDuration <= 9000:
+			two.write(path+","+transcript+","+str(duration)+"\n")
+		if totalDuration <= 18000:
+			five.write(path+","+transcript+","+str(duration)+"\n")
+		if totalDuration <= 36000:
+			ten.write(path+","+transcript+","+str(duration)+"\n")
