2b4e596ce3661d831f692628abc4da21dc727862
its-Annika <ashankwi@ehecatl.cl.indiana.edu>
2024-06-10 17:26:58 -0400

    pathing updates

--- ASR_Data2/convert.py
+++ ASR_Data2/convert.py
@@ -2,18 +2,17 @@ import sys
 import os
 import shutil
 from pathlib import Path
+import re 
 
-
-#file, name of new directory 
+#file, lang code 
 
 file = sys.argv[1]
-newDirectory = sys.argv[2]
+langCode= sys.argv[2]
 storage = {}
 
 id = 0
 #get content of the lines
 with open(file) as f:
-	next(f)
 	for line in f:
 		storage[id] = (line.split(",")[0], line.split(",")[1])
 
@@ -21,24 +20,25 @@ with open(file) as f:
 
 #make a new directory
 try:
-	os.mkdir(newDirectory)
+	os.mkdir(langCode+"ASR_Files")
 except:
 	pass
 
+# make new inner directory
+os.mkdir(langCode+"ASR_Files"+"/"+re.sub(".txt","", file))
 
 #write the new asr folders
 #one with the id and path
 #one with the id and transcript
-with open(newDirectory + "/audio_paths", "w+") as af, open(newDirectory+ "/text", "w+") as tf:
+with open(langCode+"ASR_Files"+"/"+re.sub(".txt","", file)+  "/audio_paths", "w+") as af, open(langCode+"ASR_Files"+"/"+re.sub(".txt","", file)+ "/text", "w+") as tf:
 	for key in storage.keys():
 		af.write(str(key) + " " + storage[key][0] + "\n")
 		tf.write(str(key) + " " + storage[key][1] + "\n")
 
 
-
 #move the inputed file to the new directory
 src_path = os.path.join(os.getcwd(), file)
-dst_path = os.path.join(os.getcwd(), newDirectory, file)
+dst_path = os.path.join(os.getcwd(), langCode+"ASR_Files"+"/"+re.sub(".txt","", file), file)
 os.rename(src_path, dst_path)
 
 
