ab9ed8d1ef56ea0b4c7d3c9749a0fd372c398bd1
its-Annika <ashankwi@ehecatl.cl.indiana.edu>
2024-06-17 17:49:06 -0400

    more datasets

--- ASR_Data2/convert.py
+++ ASR_Data2/convert.py
@@ -20,17 +20,17 @@ with open(file) as f:
 
 #make a new directory
 try:
-	os.mkdir(langCode+"ASR_FilesLarge")
+	os.mkdir(langCode+"ASR_FilesSmall")
 except:
 	pass
 
 # make new inner directory
-os.mkdir(langCode+"ASR_FilesLarge"+"/"+re.sub(".txt","", file))
+os.mkdir(langCode+"ASR_FilesSmall"+"/"+re.sub(".txt","", file))
 
 #write the new asr folders
 #one with the id and path
 #one with the id and transcript
-with open(langCode+"ASR_FilesLarge"+"/"+re.sub(".txt","", file)+  "/audio_paths", "w+") as af, open(langCode+"ASR_FilesLarge"+"/"+re.sub(".txt","", file)+ "/text", "w+") as tf:
+with open(langCode+"ASR_FilesSmall"+"/"+re.sub(".txt","", file)+  "/audio_paths", "w+") as af, open(langCode+"ASR_FilesSmall"+"/"+re.sub(".txt","", file)+ "/text", "w+") as tf:
 	for key in storage.keys():
 		af.write(str(key) + " " + storage[key][0] + "\n")
 		tf.write(str(key) + " " + storage[key][1] + "\n")
@@ -38,7 +38,7 @@ with open(langCode+"ASR_FilesLarge"+"/"+re.sub(".txt","", file)+  "/audio_paths"
 
 #move the inputed file to the new directory
 src_path = os.path.join(os.getcwd(), file)
-dst_path = os.path.join(os.getcwd(), langCode+"ASR_FilesLarge"+"/"+re.sub(".txt","", file), file)
+dst_path = os.path.join(os.getcwd(), langCode+"ASR_FilesSmall"+"/"+re.sub(".txt","", file), file)
 os.rename(src_path, dst_path)
 
 
